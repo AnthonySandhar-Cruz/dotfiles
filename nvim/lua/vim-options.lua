644b2501b34db766473f4a48cc3bf1cf0cfdc13a
Anthony Sandhar-Cruz <123418080+AnthonySandhar-Cruz@users.noreply.github.com>
2024-05-31 23:13:30 -0400

    Add files via upload

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,3 +5,9 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set mouse=a")
 vim.cmd("imap jj <Esc>")
 vim.g.mapleader = " "
+
+vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', {
+	expr = true,
+	replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
