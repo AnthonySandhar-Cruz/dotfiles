7efc84c274655742e31e8855bab598b2dda9f5cf
Anthony Sandhar-Cruz <123418080+AnthonySandhar-Cruz@users.noreply.github.com>
2024-05-31 23:13:03 -0400

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,3 +14,30 @@ vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
+
+-- Function to save the file and run latexmk
+local function save_and_compile()
+  -- Save the current file
+  vim.cmd('write')
+  
+  -- Get the current file name
+  local filename = vim.fn.expand('%:p')
+  
+  -- Check if the file is a .tex file
+  if string.match(filename, '%.tex$') then
+    -- Construct the command
+    local command = 'latexmk -pdf ' .. filename
+    
+    -- Run the command
+    vim.fn.system(command)
+    
+    -- Notify the user
+    print('Ran: ' .. command)
+	vim.fn.system('zathura ' .. string.gsub(filename, '%.tex$', '.pdf'))
+  else
+    print('Not a .tex file')
+  end
+end
+
+-- Create the custom command :Wc
+vim.api.nvim_create_user_command('Wc', save_and_compile, {})
